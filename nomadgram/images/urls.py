9ae11401048bcb7b908652b291d2daf34b6da72e
tonky0110 <tonky0110@gmail.com>
2018-05-03 23:02:50 +0900

    1-41) url로부터 image_id 받아오기 (urls --> views)

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -8,4 +8,18 @@ urlpatterns = [
         view=views.Feed.as_view(),
         name='feed'
     ),
+    url(
+        regex=r'(?P<image_id>\w+)/like/',
+        view=views.LikeImage.as_view(),
+        name='like_image'
+    )
 ]
+
+
+
+# /images/3/like/
+
+# 0. cate the url and the view
+# 1. take the id from the url
+# 2. we want to find an image with this id
+# 3. we want to create a like form that image
