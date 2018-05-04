a7508e6084086e2e7095c122b2cf261d1cff30db
tonky0110 <tonky0110@gmail.com>
2018-05-04 22:57:29 +0900

    1-50) unfollowing a user

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -8,9 +8,14 @@ urlpatterns = [
         view=views.ExploreUsers.as_view(),
         name="explore_users"
      ),
-     url(
+    url(
          regex=r'(?P<user_id>[0-9]+)/follow/$',
          view=views.FollowUser.as_view(),
          name='follow_user'
-     )
+     ),
+    url(
+         regex=r'(?P<user_id>[0-9]+)/unfollow/$',
+         view=views.UnFollowUser.as_view(),
+         name='follow_user'
+    )
 ]
