8df4f398b148824871844d732905cc544b85cbe7
tonky0110 <tonky0110@gmail.com>
2018-05-04 22:54:11 +0900

    1-49) following a user

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
         regex=r"^explore/$",
         view=views.ExploreUsers.as_view(),
         name="explore_users"
-     )   
+     ),
+     url(
+         regex=r'(?P<user_id>[0-9]+)/follow/$',
+         view=views.FollowUser.as_view(),
+         name='follow_user'
+     )
 ]
