2a345860db7858b06565c41489f46951e815ae98
tonky0110 <tonky0110@gmail.com>
2018-05-06 11:27:55 +0900

    1-63) image detail_등록자, 이미지, comments, like_count, created_at

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -71,5 +71,6 @@ class ImageSerializer(serializers.ModelSerializer):
             "caption",
             'comments',
             'like_count',
-            'creator'
+            'creator',
+            'created_at'
         )
