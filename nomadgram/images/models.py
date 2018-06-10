376477a728134dc35d7865645aa33f80b707ff65
tonky0110 <tonky0110@gmail.com>
2018-06-10 09:53:48 +0900

    1-35

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from nomadgram.users import models as user_models
 
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,13 +13,36 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
 
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.message
+
+
+@python_2_unicode_compatible
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
