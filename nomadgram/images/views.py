9da4da0df1e1e016649da1d6e1be9cc4a5fec98c
tonky0110 <tonky0110@gmail.com>
2018-06-10 12:15:07 +0900

    1-39
    Feed 구현
    User가 following하는 user의 최근 2개씩의 image를 불러와 시간 순으로 정렬

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -3,36 +3,26 @@ from rest_framework.response import Response
 from . import models, serializers
 
 # Create your views here.
-class ListAllImages(APIView):
-
-    def get(self, request, format=None):
-
-        all_images = models.Image.objects.all()
-
-        serializer = serializers.ImageSerializer(all_images, many=True)
-
-        return Response(data=serializer.data)
-
-
-
-class ListAllComments(APIView):
+class Feed(APIView):
 
     def get(self, request, format=None):
         
-        all_comments = models.Comment.objects.all()
+        user = request.user
 
-        serializer = serializers.CommentSerializer(all_comments, many=True)
+        following_users = user.following.all()
 
-        return Response(data=serializer.data)
+        image_list = []
 
+        for following_user in following_users:
+            
+            user_images = following_user.images.all()[:2]
 
+            for image in user_images:
+                
+                image_list.append(image)
 
-class ListAllLikes(APIView):
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
-    def get(self, request, format=None):
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
 
-        all_likes = models.Like.objects.all()
-
-        serializer = serializers.LikeSerializer(all_likes, many=True)
-
-        return Response(data=serializer.data)
+        return Response(serializer.data)
