2864ced298a6028d63d9bde2b57dc9e421da76b5
tonky0110 <tonky0110@gmail.com>
2018-06-30 23:42:55 +0900

    1-66
    image delete
    find_own_image function 생성 후 공통 소스 분리
    내가 등록한 이미지 조회 후 삭제 처리 method생성

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -186,6 +186,14 @@ class ModerateComments(APIView):
 
 
 class ImageDetail(APIView):
+    
+    def find_own_image(self, image_id, user):
+        
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
 
     def get(self, request, image_id, format=None):
 
@@ -204,10 +212,11 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            
+            return Response(status=satus.HTTP_401_UNAUTHORIZED)
         
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
 
@@ -220,3 +229,21 @@ class ImageDetail(APIView):
         else:
         
             return Response(data=serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
+        
