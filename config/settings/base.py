d6589beaec79c6abda4737bfcf10208959cd8a36
tonky0110 <tonky0110@gmail.com>
2018-07-01 22:05:23 +0900

    1-72
    ACCOUNT_EMAIL_REQUIRED = False # True
    ACCOUNT_EMAIL_VERIFICATION = 'none' # 'mandatory'
    동영상강의에는 없지만, 2번째 조건도 변경 필수
    http://localhost:8000/rest-auth/login/
    body
    username: nomadmin
    password: ....
    
    logout, registration 구현 완료

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,8 +67,11 @@ THIRD_PARTY_APPS = [
     'allauth.account', # registration
     'allauth.socialaccount', # registration
     'rest_framework', # REST framework
+    'rest_framework.authtoken',
     'taggit', # taggit for the photos
     'taggit_serializer', # tag serializer
+    'rest_auth',
+    'rest_auth.registration', # enable registration
 ]
 LOCAL_APPS = [
     'nomadgram.users.apps.UsersConfig',
@@ -96,9 +99,9 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = 'users:redirect'
+# LOGIN_REDIRECT_URL = 'users:redirect'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
-LOGIN_URL = 'account_login'
+# LOGIN_URL = 'account_login'
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
@@ -230,9 +233,9 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False # True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none' # 'mandatory'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = 'nomadgram.users.adapters.AccountAdapter'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
@@ -249,7 +252,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
+REST_USE_JWT = True
+ACCOUNT_LOGOUT_ON_GET = True
